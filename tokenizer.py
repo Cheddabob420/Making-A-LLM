8fb20cbe59c4e1ee551c0a03b8480699d3ed29db
Cody Gibson <Dntbanmethistime@yahoo.com>
2026-06-09 11:32:22 -0400

    Refactor tokenizer.py to process vocabulary size
    Comment out print statement and add vocabulary size calculation.

--- tokenizer.py
+++ tokenizer.py
@@ -13,4 +13,11 @@ text = "Hello, world. Is this-- a test?"
 
 result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
 result = [item.strip() for item in result if item.strip()]
-print(result)
+#print(result)
+preprocessed = result
+
+
+all_words = sorted(set(preprocessed))
+vocab_size = len(all_words)
+
+print(vocab_size)
